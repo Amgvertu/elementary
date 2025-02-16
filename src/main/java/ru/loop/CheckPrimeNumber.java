9b34528b4a4502615de8f6c29b0f3d60ad0b1d37
amgvertu <amgvertu@gmail.com>
2025-02-16 11:44:24 +0700

    4.8 Простое число оператор прерывания break

---
+++ src/main/java/ru/loop/CheckPrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = true;
+        for (int i = 1; i <= number; i++) {
+            if (((number % i == 0) && (i > 1) && (i != number)) || (number == 1)) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(check(1));
+    }
+
+}
