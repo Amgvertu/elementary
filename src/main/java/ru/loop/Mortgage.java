a4ed10d37c4d91f36e7171d2074ef10c556870d6
Amgvertu <amgvertu@gmail.com>
2025-02-18 15:39:29 +0700

    4.10 Ипотека

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -7,7 +7,7 @@ public class Mortgage {
 
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        while (amountChange(amount, percent) > 0) {
+        while (amount > 0) {
             amount = amountChange(amount, percent) - salary;
             year++;
         }
