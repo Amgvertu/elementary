2a0c0ed2defa0f6e140442c00fe17580bce984db
Amgvertu <amgvertu@gmail.com>
2025-02-18 12:00:53 +0700

    4.10 Ипотека

---
+++ src/main/java/ru/loop/Mortgage.java
@@ -0,0 +1,13 @@
+package ru.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 1;
+        while (amount * (1 + percent / 100) > salary) {
+            amount = amount * (1 + percent / 100) - salary;
+            year++;
+        }
+        return year;
+    }
+}
+
