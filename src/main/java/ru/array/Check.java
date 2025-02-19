1c991fa1e334512279902d8f4efe56c7ed554ecb
Amgvertu <amgvertu@gmail.com>
2025-02-19 16:33:56 +0700

    5.13 Массив заполнен true или false

---
+++ src/main/java/ru/array/Check.java
@@ -0,0 +1,15 @@
+package ru.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        for (boolean element : data) {
+            if (data[0] != element) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
+
