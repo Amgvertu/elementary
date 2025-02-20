b423d2182d781f1581c78d1cc24c03112bb04fba
amgvertu <amgvertu@gmail.com>
2025-02-20 23:23:40 +0700

    5.16 Обход массива с последнего элемента

--- src/main/java/ru/array/RollBackArray.java
+++ src/main/java/ru/array/RollBackArray.java
@@ -3,7 +3,7 @@ package ru.array;
 public class RollBackArray {
     public static int[] rollback(int[] array) {
         int[] result = new int[array.length];
-        for (int i = array.length - 1; i >= 0; i--) {
+        for (int i = 0; i < array.length; i++) {
             result[i] = array[array.length - 1 - i];
         }
         return result;
