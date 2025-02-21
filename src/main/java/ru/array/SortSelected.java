c8aba7b5af6f98d4b53876dc6185440cac686526
Amgvertu <amgvertu@gmail.com>
2025-02-21 14:50:12 +0700

    5.21 Сортировка выборкой

--- src/main/java/ru/array/SortSelected.java
+++ src/main/java/ru/array/SortSelected.java
@@ -2,23 +2,12 @@ package ru.array;
 
 public class SortSelected {
     public static int[] sort(int[] data) {
-        int min, ind, temp;
-        for (int i = 0; i < data.length - 1; i++) {
-            min = data[i];
-            ind = i;
-            for (int j = i + 1; j < data.length; j++) {
-                if (data[j] < min) {
-                    min = data[j];
-                    ind = j;
-                }
-            }
-            if (ind != i) {
-                temp = data[ind];
-                data[ind] = data[i];
-                data[i] = temp;
-            }
+        int min, index;
+        for (int i = 0; i < data.length; i++) {
+            min = MinDiapason.findMin(data, i, data.length - 1);
+            index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            SwitchArray.swap(data, i, index);
         }
         return data;
     }
-
 }
