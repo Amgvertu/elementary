2d84947ca9cf67ab728fb8c813fd30f067bb07cc
amgvertu <amgvertu@gmail.com>
2025-02-28 00:10:36 +0700

    5.32 Жадный алгоритм. Сдача в кофе

--- src/main/java/ru/array/Machine.java
+++ src/main/java/ru/array/Machine.java
@@ -8,11 +8,10 @@ public class Machine {
         int[] rsl = new int[100];
         int size = 0;
         money -= price;
-        for (int i = 0; i < coins.length; i++) {
-            while (money >= coins[i]) {
-                rsl[size] = coins[i];
-                money -= coins[i];
-                size++;
+        for (int change : coins) {
+            while (money >= change) {
+                rsl[size++] = change;
+                money -= change;
             }
         }
         return Arrays.copyOf(rsl, size);
