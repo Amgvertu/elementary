5fbc2a64098e1f82b87e10e3ca94ce620a2c739d
amgvertu <amgvertu@gmail.com>
2025-05-05 09:45:52 +0700

    7.13 Рефакторинг. Расстояние между точками

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -7,34 +7,28 @@ class PointTest {
     @Test
     void when00to20then2() {
         double expected = 2;
-        int x1 = 0;
-        int y1 = 0;
-        int x2 = 2;
-        int y2 = 0;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(0, 0);
+        Point b = new Point(2, 0);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
     @Test
     void when52to151then10dot049() {
         double expected = 10.049;
-        int x1 = 5;
-        int y1 = 2;
-        int x2 = 15;
-        int y2 = 1;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.001));
+        Point a = new Point(5, 2);
+        Point b = new Point(15, 1);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.001));
     }
 
     @Test
     void when17to23then6dot08() {
         double expected = 4.12;
-        int x1 = 1;
-        int y1 = 7;
-        int x2 = 2;
-        int y2 = 3;
-        double out = Point.distance(x1, y1, x2, y2);
-        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+        Point a = new Point(1, 7);
+        Point b = new Point(2, 3);
+        double dist = a.distance(b);
+        assertThat(dist).isEqualTo(expected, withPrecision(0.01));
     }
 
 }
