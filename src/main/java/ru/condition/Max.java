46b95bc6f2520d8a8a41118335921b35c306723b
amgvertu <amgvertu@gmail.com>
2025-05-06 10:58:53 +0700

    7.15 Перегрузить метод max для трех чисел

--- src/main/java/ru/condition/Max.java
+++ src/main/java/ru/condition/Max.java
@@ -1,13 +1,21 @@
 package ru.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        return left >= right ? left : right;
+    public static int max(int first, int second) {
+        return first >= second ? first : second;
+    }
+
+    public static int max(int first, int second, int third) {
+        return first >= max(second, third) ? first : max(second, third);
+    }
+
+    public static int max(int first, int second, int third, int fourth) {
+        return first >= max(second, third, fourth) ? first : max(second, third, fourth);
     }
 
     public static void main(String[] args) {
         System.out.println(max(2, 5));
-        System.out.println(max(4, 1));
-        System.out.println(max(9, 9));
+        System.out.println(max(12, 5, 7));
+        System.out.println(max(3, 14, 7, 9));
     }
 }
--- src/test/java/ru/condition/MaxTest.java
+++ src/test/java/ru/condition/MaxTest.java
@@ -8,29 +8,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MaxTest {
     @Test
-    void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
-        int expected = 2;
+    void whenMax2To5Then5() {
+        int first = 2;
+        int second = 5;
+        int result = Max.max(first, second);
+        int expected = 5;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenMax4To1Then4() {
-        int left = 4;
-        int right = 1;
-        int result = Max.max(left, right);
-        int expected = 4;
+    void whenMax12To5to7Then12() {
+        int first = 12;
+        int second = 5;
+        int third = 7;
+        int result = Max.max(first, second, third);
+        int expected = 12;
         assertThat(result).isEqualTo(expected);
     }
 
     @Test
-    void whenMax7To7Then7() {
-        int left = 7;
-        int right = 7;
-        int result = Max.max(left, right);
-        int expected = 7;
+    void whenMax3To14to7to9Then14() {
+        int first = 3;
+        int second = 14;
+        int third = 7;
+        int fourth = 9;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 14;
         assertThat(result).isEqualTo(expected);
     }
 }
